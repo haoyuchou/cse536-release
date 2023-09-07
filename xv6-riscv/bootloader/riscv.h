a1a9aac06ecd447b1fad9f6a4b6421c475a1f1ad
haoyuchou <haoyuchou123@gmail.com>
2023-09-07 16:59:46 -0700

    add static inline void to eacg w_pmpaddr

--- xv6-riscv/bootloader/riscv.h
+++ xv6-riscv/bootloader/riscv.h
@@ -196,21 +196,25 @@ w_pmpaddr0(uint64 x)
   asm volatile("csrw pmpaddr0, %0" : : "r" (x));
 }
 
+static inline void
 w_pmpaddr1(uint64 x)
 {
   asm volatile("csrw pmpaddr1, %0" : : "r" (x));
 }
 
+static inline void
 w_pmpaddr2(uint64 x)
 {
   asm volatile("csrw pmpaddr2, %0" : : "r" (x));
 }
 
+static inline void
 w_pmpaddr3(uint64 x)
 {
   asm volatile("csrw pmpaddr3, %0" : : "r" (x));
 }
 
+static inline void
 w_pmpaddr4(uint64 x)
 {
   asm volatile("csrw pmpaddr4, %0" : : "r" (x));
