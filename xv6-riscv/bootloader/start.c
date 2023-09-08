16762ac62841cdcf3ab4324af30f819e1c8fb1e2
haoyuchou <haoyuchou123@gmail.com>
2023-09-07 17:29:56 -0700

    update w_pmpaddr1 address

--- xv6-riscv/bootloader/start.c
+++ xv6-riscv/bootloader/start.c
@@ -132,10 +132,10 @@ void start()
     // 118-120MB, isolated
     w_pmpaddr1(0x21DBFFFF);
     // 120-122MB, start 120MB, decimal: 2273312768
-    w_pmpaddr2(0x21E3FFFF);
+    w_pmpaddr2(0x87BFFFF);
     // 122-126MB, isolated, start 122MB
     w_pmpaddr3(0x43D7FFFF);
-    //126-128MB
+    //126-128MB, start 126MB
     w_pmpaddr4(0x21FBFFFF);
     // 0f: read, write, execute, f is 16, which means have all access, 18, 1 means use NAPTO entry
     w_pmpcfg0(0x1f181f180f);
